78325e8cd99313492e592e5bfe3c544d5e8cb282
liarch29 <charliean19@gmail.com>
2019-05-06 23:47:45 -0700

    hash bug fix

--- HashSet.cpp
+++ HashSet.cpp
@@ -14,17 +14,17 @@ HashSet::HashSet(){
 
 HashSet::~HashSet(){
 	for (int i = 0; i < nslots; i++) {
-		std::cout << *slots[index] << std::endl;
+		delete (slots[i]);
 	}
 }
 
 void HashSet::insert(const std::string & value){
-	if (nitems > nslots) {
+	if (nitems == nslots) {
 		std::string** slots_storage = this->slots;
 
 		nslots *= 2;
 		nitems = 0;
-		this->intfn = new SquareRootHash(0, nslots);
+		this->intfn = new SquareRootHash(1, nslots);
 		this->slots = new std::string*[nslots];
 
 		for(int i = 0; i < nslots / 2; i++) {
