762c46669dbf37e0b489ad8cb514d9af582f7538
TropicMango <charliean19@gmail.com>
2019-05-06 21:12:13 -0700

    Update HashSet.cpp

--- HashSet.cpp
+++ HashSet.cpp
@@ -3,15 +3,36 @@ void HashSet::rehash() {
 }
 
 HashSet::HashSet(){
-	nslots = 1;
+	nslots = 64;
 	nitems = 0;
-	this->intfn = new SquareRootHash();
+	this->intfn = new SquareRootHash(0, nslots);
+	this->strfn = new JenkinsHash();
+	this->strfn2 = new PearsonHash();
+	this->slots = new string*[nslots];
 }
 
 HashSet::~HashSet(){
 }
 
 void HashSet::insert(const std::string & value){
+	if (nitems > nslots) {
+		string** slots_storage = this->slots;
+
+		nslots *= 2;
+		this->intfn = new SquareRootHash(0, nslots);
+		this->slots = new string*[nslots];
+
+		for each (string* str in slots_storage) {
+			this->insert(str);
+		}
+	}
+	nitems++;
+
+	uint64_t index = intfn->hash(strfn->hash(value));
+	while (*slots[index] != NULL)
+		index++;
+	
+	*slots[index] = value;
 }
 
 bool HashSet::lookup(const std::string & value) const{
